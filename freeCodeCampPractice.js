7360cf0efb7493e8e07ac7d5a758fd18910c0882
VishwanathSinghYadav <singhvishwanathyadav@gmail.com>
2022-01-25 17:43:28 +0530

    practice 1

---
+++ freeCodeCampPractice.js
@@ -0,0 +1,34 @@
+var basicJavascript;
+
+{
+    // Comment Your JavaScript Code
+    {
+        // This is an in-line comment.
+        /* This is a
+        multi-line comment */
+    }
+
+    // Declare JavaScript Variables
+    {
+        /* 
+        JavaScript provides eight different data types 
+        undefined, 
+        null, 
+        boolean, 
+        string, 
+        symbol, 
+        bigint, 
+        number,
+        object 
+        */
+
+        var ourName; //We tell JavaScript to create or declare a variable by putting the keyword var in front of it
+    }
+
+    // Storing Values with the Assignment Operator
+    {
+        var myVar;
+        myVar = 5;
+    }
+
+}
